3bc9f978c83fa65ce27d125cf344711f2b3cc9c7
Sergey <137205761+sergeychovgun@users.noreply.github.com>
2026-05-03 22:15:07 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://pixabay.com/api/";
-const API_KEY = "ТУТ_ТВІЙ_KEY";
+const API_KEY = "55702629-61a52a53581ffbc81075bfc8f";
 
 export function getImagesByQuery(query) {
   const params = {
